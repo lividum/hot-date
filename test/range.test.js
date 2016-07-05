362ab2648c6a6d7a8eee419a106cba3643a76cab
Andi Nugroho Dirgantara <andi.n.dirgantara@gmail.com>
2016-07-05 23:00:49 +0700

    add more test

---
+++ test/range.test.js
@@ -0,0 +1,64 @@
+import Range from '../lib/range';
+import Util from '../lib/util';
+import moment from 'moment-timezone';
+
+describe('Range Library', () => {
+  it('able to create basic range', () => {
+    let list;
+
+    const start = '2016-01-01';
+    const end = '2016-02-01';
+    const format = 'YYYY-MM-DD';
+
+    const range = new Range(start, end);
+
+    list = range.interval().toArray();
+    list.length.should.equal(32);
+    list[0].toMoment().format(format).should.be.equal(start);
+    list[list.length - 1].toMoment().format(format).should.be.equal(end);
+
+    list = range.interval('M').toArray();
+    list.length.should.equal(2);
+    list[0].toMoment().format(format).should.be.equal(start);
+    list[list.length - 1].toMoment().format(format).should.be.equal(end);
+
+    list = range.interval('Y').toArray();
+    list.length.should.equal(1);
+    list[0].toMoment().format(format).should.be.equal(start);
+
+    list = range.interval('H').toArray();
+    list.length.should.equal(745);
+    list[0].toMoment().format(`${format} HH:mm`).should.be.equal(`${start} 00:00`);
+    list[list.length - 1].toMoment().format(`${format} HH:mm`).should.be.equal(`${end} 00:00`);
+
+    list = range.interval('m').toArray();
+    list.length.should.equal(44641);
+  });
+
+  it('able to create basic range with interval', () => {
+    let list;
+
+    const range = new Range('2016-01-01', '2016-02-01');
+
+    list = range.interval('day', 2).toArray();
+    list.length.should.equal(16);
+
+    list = range.interval('M', 2).toArray();
+    list.length.should.equal(1);
+  });
+
+  it('able to convert to moment and date list', () => {
+    let list;
+
+    const range = new Range('2016-01-01', '2016-02-01');
+
+    list = range.interval().toArray().map(li => (li instanceof Util).should.equal(true));
+    list.length.should.equal(32);
+
+    list = range.interval().toMomentArray().map(li => (li instanceof moment).should.equal(true));
+    list.length.should.equal(32);
+
+    list = range.interval().toDateArray().map(li => (li instanceof Date).should.equal(true));
+    list.length.should.equal(32);
+  });
+});
---
+++ test/util.test.js
@@ -0,0 +1,152 @@
+import Util from '../lib/util';
+import moment from 'moment-timezone';
+
+describe('Util Library', () => {
+  it('able to parse string with default format', () => {
+    const mom = new Util('2015-03-01').toMoment();
+
+    mom.format('z').should.be.equal('WIB');
+    mom.format('DD').should.be.equal('01');
+    mom.format('MM').should.be.equal('03');
+    mom.format('YYYY').should.be.equal('2015');
+  });
+
+  it('able to parse string with default format but different non-alphanumberic ', () => {
+    const mom = new Util('2015/03/01').toMoment();
+
+    mom.format('z').should.be.equal('WIB');
+    mom.format('DD').should.be.equal('01');
+    mom.format('MM').should.be.equal('03');
+    mom.format('YYYY').should.be.equal('2015');
+  });
+
+  it('able to parse string with custom format', () => {
+    const format = 'YYYY-MM-DD HH:mm';
+    const mom = new Util('2015-03-01 15:03', format).toMoment();
+
+    mom.format('z').should.be.equal('WIB');
+    mom.format('DD').should.be.equal('01');
+    mom.format('MM').should.be.equal('03');
+    mom.format('YYYY').should.be.equal('2015');
+    mom.format('HH').should.be.equal('15');
+    mom.format('mm').should.be.equal('03');
+  });
+
+  it('able to parse string with custom format from static method', () => {
+    const format = 'YYYY-MM-DD HH:mm';
+    const mom = Util.fromString('2015-03-01 15:03', format).toMoment();
+
+    mom.format('z').should.be.equal('WIB');
+    mom.format('DD').should.be.equal('01');
+    mom.format('MM').should.be.equal('03');
+    mom.format('YYYY').should.be.equal('2015');
+    mom.format('HH').should.be.equal('15');
+    mom.format('mm').should.be.equal('03');
+  });
+
+  it('throw error when given string is not valid', () => {
+    /* eslint no-undef: 0 */
+    expect(() => new Util('string that is not date')).to.throw(Error);
+  });
+
+  it('easy to transform from date to moment and vice versa', () => {
+    const input = '2013-01-23';
+    const obj = new Util(input);
+
+    const date = obj.toDate();
+    Util.validate(date).should.be.equal(true);
+    (date instanceof Date).should.be.equal(true);
+
+    const mom = obj.toMoment();
+    mom.format('YYYY-MM-DD').should.be.equal(input);
+    (mom instanceof moment).should.be.equal(true);
+  });
+
+  it('able to create date Util from date Util', () => {
+    const date = new Util('2016-01-01');
+    const date2 = new Util(date);
+
+    (date.toDate().getTime() === date2.toDate().getTime()).should.be.equal(true);
+  });
+
+  it('able to modify moment object', () => {
+    const mom = new Util('2012-03-04').toMoment(mom2 => mom2.add(1, 'd')).toMoment();
+
+    mom.format('DD').should.be.equal('05');
+  });
+
+  it('able to modify date object', () => {
+    const mom = new Util('2012-03-04').toDate(date2 => {
+      date2.setMonth(0);
+      return date2;
+    }).toMoment();
+
+    mom.format('MM').should.be.equal('01');
+  });
+
+  it('able to get start of time', () => {
+    let mom;
+
+    const date = new Util('2015-02-03 15:09', 'YYYY-MM-DD HH:mm');
+    mom = date.startOf('Y').toMoment();
+
+    mom.format('YYYY').should.equal('2015');
+    mom.format('MM').should.equal('01');
+    mom.format('DD').should.equal('01');
+    mom.format('HH').should.equal('00');
+    mom.format('mm').should.equal('00');
+    mom.format('s').should.equal('0');
+    mom.format('z').should.equal('WIB');
+
+    // invalid moment
+    mom = date.startOf('just string').toMoment();
+
+    mom.format('YYYY').should.equal('2015');
+    mom.format('MM').should.equal('02');
+    mom.format('DD').should.equal('03');
+    mom.format('HH').should.equal('15');
+    mom.format('mm').should.equal('09');
+    mom.format('s').should.equal('0');
+    mom.format('z').should.equal('WIB');
+  });
+
+  it('able to get end of time', () => {
+    let mom;
+
+    const date = new Util('2015-02-03 15:09', 'YYYY-MM-DD HH:mm');
+
+    mom = date.endOf('Y').toMoment();
+
+    mom.format('YYYY').should.equal('2015');
+    mom.format('MM').should.equal('12');
+    mom.format('DD').should.equal('31');
+    mom.format('HH').should.equal('23');
+    mom.format('mm').should.equal('59');
+    mom.format('s').should.equal('59');
+    mom.format('z').should.equal('WIB');
+
+    // invalid moment
+    mom = date.endOf('just string').toMoment();
+
+    mom.format('YYYY').should.equal('2015');
+    mom.format('MM').should.equal('02');
+    mom.format('DD').should.equal('03');
+    mom.format('HH').should.equal('15');
+    mom.format('mm').should.equal('09');
+    mom.format('s').should.equal('0');
+    mom.format('z').should.equal('WIB');
+  });
+
+  describe('Pre defined string', () => {
+    it('able to parse yesterday', () => {
+      const now = moment();
+
+      const mom = new Util('yesterday').toMoment();
+
+      mom.format('z').should.be.equal('WIB');
+      mom.format('DD').should.be.equal(now.add(-1, 'd').format('DD'));
+      mom.format('MM').should.be.equal(now.format('MM'));
+      mom.format('YYYY').should.be.equal(now.format('YYYY'));
+    });
+  });
+});
